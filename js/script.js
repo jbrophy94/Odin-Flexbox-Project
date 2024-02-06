63a728e0f87c70e886fd74bf7b60218d330de9a0
Jack Brophy <jackbrophy5@gmail.com>
2024-02-06 08:51:13 -0500

    Added alert functionality to sign up buttons using JS.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use-strict'
+
+topSignUpButton = document.querySelector(".hero button")
+bottomSignUpButton = document.querySelector(".body-section5 button")
+
+topSignUpButton.addEventListener('click', function(){
+  alert("Unfortunately there is nothing to really sign up for since this is a cat and paint store.")
+})
+bottomSignUpButton.addEventListener('click', function(){
+  alert("Thanks for answering the call!")
+})
